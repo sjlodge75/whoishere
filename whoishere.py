49c75012ec7abe996bea80ec30735c254742853a
sjlodge75 <36677165+sjlodge75@users.noreply.github.com>
2018-02-20 20:34:41 +0000

    Add files via upload
    Works in local host (Python IDLE runtime)

--- whoishere.py
+++ whoishere.py
@@ -6,9 +6,8 @@ def main():
     conn = urllib2.urlopen("http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
                            % (CHANNEL_ID,READ_API_KEY))
     response = conn.read()
-    print "http status code=%s" % (conn.getcode())
     data=json.loads(response)
-    print data['field2'],data['created_at']
+    print data['field2']
     conn.close()
 if __name__ == '__main__':
     main()
