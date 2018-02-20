fffd82c07bc2b533dcbf57e014c32e84c77c3b76
sjlodge75 <36677165+sjlodge75@users.noreply.github.com>
2018-02-20 21:10:45 +0000

    Add files via upload
    Reverting to Python2 libraries.

--- whoishere.py
+++ whoishere.py
@@ -1,11 +1,11 @@
 
-import json
-import urllib.request as ur
+import urllib2,json
+
 
 READ_API_KEY='F4OA1IJI4U0BMH3E'
 CHANNEL_ID='420847'
 def main():
-    conn = ur.urlopen("http://api.thingspeak.com/channels/" + CHANNEL_ID + "/feeds/last.json?api_key=" + READ_API_KEY)
+    conn = urllib2.urlopen("http://api.thingspeak.com/channels/" + CHANNEL_ID + "/feeds/last.json?api_key=" + READ_API_KEY)
     response = conn.read()
     data=json.loads(response)
     #print data['field2'] #Works in command line. Try 'return' for web
