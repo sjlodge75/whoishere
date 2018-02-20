e696f78238f45c35576d84aef719c135c7fff399
sjlodge75 <36677165+sjlodge75@users.noreply.github.com>
2018-02-20 20:56:01 +0000

    Add files via upload
    Corrected library syntax

--- whoishere.py
+++ whoishere.py
@@ -1,10 +1,11 @@
 
-import urllib,json
+import json
+import urllib.request as ur
 
 READ_API_KEY='F4OA1IJI4U0BMH3E'
 CHANNEL_ID=420847 
 def main():
-    conn = urllib.urlopen("http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
+    conn = ur.urlopen("http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
                            % (CHANNEL_ID,READ_API_KEY))
     response = conn.read()
     data=json.loads(response)
@@ -13,3 +14,9 @@ def main():
     conn.close()
 if __name__ == '__main__':
     main()
+
+	
+	
+
+
+
