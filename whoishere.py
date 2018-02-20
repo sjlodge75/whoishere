58043d97119532e4ce2a6faeb4cec6a819709879
sjlodge75 <36677165+sjlodge75@users.noreply.github.com>
2018-02-20 20:07:10 +0000

    Add files via upload

---
+++ whoishere.py
@@ -0,0 +1,158 @@
+
+"""
+This skill returns data from a thinkspeak channel.
+
+"""
+
+from __future__ import print_function
+import urllib
+
+# Change these elements to point to your data
+READ_API_KEY='F4OA1IJI4U0BMH3E'
+channel = 420847
+field = 2
+#
+
+link = "https://api.thingspeak.com/channels/" + str(channel) + "/fields/" + str(field) + "/last.json?api_key=" +str(READ_API_KEY)
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "SpeedTestIntent":
+        return run_speed_test(intent, session, link)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+# --------------- Functions that control the skill's behavior ------------------
+def get_welcome_response():
+session_attributes = {}
+card_title = “Welcome”
+speech_output = “Steve is here in the house”
+should_end_session = True
+return build_response(session_attributes, build_speechlet_response(card_title, speech_output, None, should_end_session))
+
+def old_get_welcome_response():
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Welcome. " \
+                    "I can tell you who is in your house " \
+                    "Just ask me to check."
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Ask me, who is in the house. "
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "I hope you haven't lost anyone."
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+# Run the Speed Test
+def run_speed_test(intent, session, link):
+    session_attributes = {}
+    reprompt_text = None
+    should_end_session = False
+    
+    f = urllib.urlopen(link) # Get your data
+    result = f.read()
+    
+    speech_output = "Your last speed was " + \
+                    result + \
+                    " megabits per second."
+                    
+       
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
+        },
+        'reprompt': {
+           'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
